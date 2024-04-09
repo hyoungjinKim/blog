2dcc6afee593b68a0cc9f84c24c261716d6734b7
HYJIN\isacc <isacc7224@gmail.com>
2024-04-09 17:17:00 +0900

    mngoose Schema 생성 및 서버 연결

--- server/app.js
+++ server/app.js
@@ -1,5 +1,13 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import config from "./config";
 const app = express();
+const { MONGO_URI } = config;
 
+mongoose
+  .connect(MONGO_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => {
+    console.log(err);
+  });
 export default app;
---
+++ server/models/user.js
@@ -0,0 +1,50 @@
+import moment from "moment";
+import mongoose from "mongoose";
+
+//create schema
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ["MainJuin", "SubJuin", "User"],
+    default: "User",
+  },
+  register_date: {
+    type: Date,
+    default: moment().format("YYYY_MM_DD hh:mm"),
+  },
+  comments: [
+    {
+      post_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "posts",
+      },
+      comment_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "comments",
+      },
+    },
+  ],
+  posts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "posts",
+    },
+  ],
+});
+
+const User = mongoose.model("user", UserSchema);
+
+export default User;
